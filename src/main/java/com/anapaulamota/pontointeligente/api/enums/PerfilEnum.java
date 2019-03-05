8be30537d0d669e36e821e55cb81e6070b578bf5
Red Head <45854953+anapaulamota@users.noreply.github.com>
2019-03-05 19:28:54 -0300

    Dependencia Flyway, entities e enums

---
+++ src/main/java/com/anapaulamota/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,11 @@
+package com.anapaulamota.pontointeligente.api.enums;
+
+/**
+ * Representa o perfil do usuário no sistema
+ * @author anamota
+ *
+ */
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/java/com/anapaulamota/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,16 @@
+package com.anapaulamota.pontointeligente.api.enums;
+
+/**
+ * Representa o tipo de lançamento	
+ * @author anamota
+ *
+ */
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+
+}
