fc5e4b1327f534490be0b11467d48c838c261a3e
Red Head <45854953+anapaulamota@users.noreply.github.com>
2019-03-09 19:49:46 -0300

    Adição de uma dependencia no pom.xml para corrigir o problema de timezone e o script sql para criação das tabelas

--- src/main/java/com/anapaulamota/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/anapaulamota/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,10 +1,20 @@
 package com.anapaulamota.pontointeligente.api;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PontoInteligenteApplication {
+	
+	@PostConstruct
+    void started() {
+      TimeZone.setDefault(TimeZone.getTimeZone("TimeZone"));
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
